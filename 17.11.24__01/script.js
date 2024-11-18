de4cd5be4a9a21015d90d65c150ffa966e616623
molnia_grom <mgrom250@gmail.com>
2024-11-18 09:54:52 +0200

    learning

---
+++ 17.11.24__01/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// alert("Привет, мир!");
+
+// let num;
+// num = 123;
+
+// alert(num);
+
+// different ways to declare a variable
+
+// let a = 2,
+//   b = 3,
+//   c = 4;
+
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+// let a;
+// let b;
+// let c;
+
+// a = 1
+// b = 2
+// c = 3
+
+// let a = 10;
+
+// alert(a);
+
+// a = 20;
+
+// alert(a);
+
+// Mathematical operations with numbers in JavaScript
+
+// let a = 1 + 2 + 3;
+// alert(a);
+
+// №1
+// let a = 10;
+// let b = 2;
+
+// alert(a + b)
+// alert(a - b)
+// alert(a * b)
+// alert(a / b)
+
+// №2
+// let c = 10;
+// let d = 5;
+// let result = c + d;
+
+// alert(result)
+
+// №3
+
