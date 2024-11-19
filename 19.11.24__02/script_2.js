bdac59ef6cde83cbabe08135ac032f72d96e21df
molnia_grom <mgrom250@gmail.com>
2024-11-19 16:29:49 +0200

    Passed the arrays

---
+++ 19.11.24__02/script_2.js
@@ -0,0 +1,64 @@
+"use strict";
+// Массивы
+
+// let arrNum = [1, 2, 3, 4, 5, 6, 7];
+// let arrStr = ["sssss", "fffff", "kkkkk"];
+// alert(arrNum);
+// console.log(arrNum);
+// alert(arrStr);
+// console.log(arrStr);
+
+// let arr = ["a", "b", "c"];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// alert(arr)
+// alert(`${arr[0]}+${arr[1]}+${arr[2]}+${arr[3]}`);
+
+// let arrNum = [];
+// arrNum[0] = 1;
+// arrNum[1] = 2;
+// arrNum[2] = 3;
+// arrNum[3] = 4;
+// arrNum[4] = 5;
+
+// arrNum[0]++;
+// arrNum[1]++;
+// arrNum[2]++;
+
+// arrNum[0] += 3;
+// arrNum[1] += 3;
+// arrNum[2] += 3;
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let key1 = 1;
+// let key2 = 2;
+
+// arr.push(4)
+// arr.push(5)
+
+// arr[3] = "a";
+// arr[8] = "b";
+// alert(arr.length);
+// console.log(arr[key1] + arr[key2]);
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// delete arr[1]
+// delete arr[3]
+
+// console.log(arr);
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length - 1]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
