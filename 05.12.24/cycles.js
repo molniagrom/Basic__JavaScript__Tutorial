16065d3804be2ce896df1f9fd625d4d6fcecc14b
dolph <mgrom250@gmail.com>
2024-12-05 08:15:24 +0200

    start learning cycles

---
+++ 05.12.24/cycles.js
@@ -0,0 +1,47 @@
+// forOf - for array
+
+/*let arr = ['a', 'b', 'c', 'd', 'e'];
+
+for (let elem of arr) {
+    console.log(elem);
+}*/
+
+//....................................
+
+// forIn - for object
+/*
+let obj = {x: 1, y: 2, z: 3};*/
+
+/*for (let key in obj) {
+    console.log(key);
+}*/
+
+/*for (let objKey in obj) {
+    console.log(obj[objKey]);
+}*/
+
+//....................................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
