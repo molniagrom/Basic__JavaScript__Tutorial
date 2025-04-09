d4b8bf178b7023c2d8b5f9c1e53de47e82228539
molnia_grom <mgrom250@gmail.com>
2025-04-09 14:41:23 +0300

    09.04.25 (3)

---
+++ 09.04.25/multidimensionality.js
@@ -0,0 +1,239 @@
+// let arr = [
+//     ['a', 'b', 'c'],
+//     ['d', 'e', 'f'],
+//     ['g', 'h', 'i'],
+//     ['j', 'k', 'l'],
+// ];
+//
+// console.log(arr[3][2]);
+// console.log(arr[1][1]);
+// console.log(arr[2][0]);
+// console.log(arr[0][0]);
+
+//.........................................................
+
+// let arr = [
+//     [1, 2],
+//     [3, 4],
+//     [5, 6]
+// ];
+//
+// let sum = 0;
+//
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         sum += arr[i][j];
+//     }
+// }
+//
+// console.log("Sum:", sum);
+
+//.........................................................
+
+// let arr = [
+//     [
+//         [1, 2],
+//         [3, 4],
+//     ],
+//     [
+//         [5, 6],
+//         [7, 8],
+//     ],
+// ];
+//
+// let sum = 0;
+//
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         for (let k = 0; k < arr[i][j].length; k++) {
+//             sum += arr[i][j][k];
+//         }
+//     }
+// }
+//
+// console.log("Sum:", sum);
+
+//.........................................................
+
+// let arr = [
+//     [1, 2, 3,
+//         [4, 5,
+//             [6, 7]
+//         ]
+//     ],
+//     [8,
+//         [9, 10]
+//     ]
+// ];
+//
+// console.log(
+//     arr[0][0] +
+//     arr[0][1] +
+//     arr[0][2] +
+//     arr[0][3][0] +
+//     arr[0][3][1] +
+//     arr[0][3][2][0] + // Accessing 6
+//     arr[0][3][2][1] + // Accessing 7
+//     arr[1][0] +      // Accessing 8
+//     arr[1][1][0] +    // Accessing 9
+//     arr[1][1][1]     // Accessing 10
+// ); // Output: 55
+
+//..........................................
+
+// let arr = [[1, 2, 3, 4, 5], [6,
+//     7, 8], [9, 10]];
+//
+// for (let subArr of arr) {
+//     for (let elem of subArr) {
+//         console.log(elem);
+//     }
+// }
+
+// let arr = [[1, 2, 3], [4, 5], [6]];
+// let sum = 0;
+//
+// for (let subArr of arr) {
+//     for (let elem of subArr) {
+//         sum += elem;
+//     }
+// }
+// console.log(sum)
+
+
+// let arr = [
+//     [
+//         [1, 2],
+//         [3, 4]
+//     ],
+//     [
+//         [5, 6],
+//         [7, 8]
+//     ]
+// ];
+//
+// let sum = 0;
+//
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         for (let k = 0; k < arr[i][j].length; k++) {
+//             sum += arr[i][j][k];
+//         }
+//     }
+// }
+//
+// console.log("Sum:", sum);
+
+// let arr = [[1, 2, 3], [4, 5], [6]];
+//
+// let sum = 0;
+//
+// for (i = 0; i < arr.length; i++) {
+//     for (j = 0; j < arr[i].length; j++) {
+//         sum += arr[i][j];
+//     }
+// }
+//
+// console.log(sum)
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = [];
+//     for (let j = 0; j < 2; j++) {
+//         arr[i][j] = [];
+//         for (let k = 1; k <= 5; k++) {
+//             arr[i][j][k] = k
+//         }
+//     }
+// }
+//
+// console.log(arr)
+
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = []; // исправила
+//     for (let j = 1; j <= 5; j++) {
+//         arr[i].push(j);
+//     }
+// }
+//
+// console.log(arr);
+
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = [];
+//
+//     for (let j = 1; j <= 5; j++) {
+//         arr[i].push(j);
+//     }
+// }
+//
+// console.log(arr);
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = []
+//
+//     for (let j = 1; j <= 5; j++) {
+//         arr[i].push(j);
+//     }
+// }
+//
+// console.log(arr);
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = [];
+//
+//     for (let j = 1; j <= 5; j++) {
+//         arr[i].push(j);
+//     }
+// }
+//
+// console.log(arr);
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = [];
+//
+//     for (let j = 1; j <= 5; j++) {
+//         arr[i].push(j);
+//     }
+// }
+//
+// console.log(arr);
+
+// let arr = [];
+//
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = [];
+//
+//     for (let j = 1; j <= 5; j++) {
+//         arr[i].push(j);
+//     }
+// }
+//
+// console.log(arr);
+
+//.....................................................................
+
+
+
+
+
+
+
+
+
+
+
+
+
