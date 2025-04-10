b07af64da08a78863268b02b2de8199ef8efb404
molnia_grom <mgrom250@gmail.com>
2025-04-10 12:54:51 +0300

    Multidimensional object enumeration in JavaScript

--- 09.04.25/multidimensionality.js
+++ 09.04.25/multidimensionality.js
@@ -361,26 +361,93 @@
 //
 // console.log(arr);
 
-let arr = [];
+// let arr = [];
+//
+// for (let i = 0, k = 1; i < 3;
+//      i++) {
+//     arr[i] = [];
+//
+//     for (let j = 0; j < 3; j++) {
+//         arr[i][j] = k++;
+//     }
+// }
+//
+// console.log(arr);
 
-for (let i = 0, k = 1; i < 3;
-     i++) {
-    arr[i] = [];
+//............................................................
 
-    for (let j = 0; j < 3; j++) {
-        arr[i][j] = k++;
-    }
-}
+// let obj = {
+//     key1: {
+//         key1: 1,
+//         key2: 2,
+//         key3: 3,
+//     },
+//     key2: {
+//         key1: 4,
+//         key2: 5,
+//         key3: 6,
+//     },
+//     key3: {
+//         key1: 7,
+//         key2: 8,
+//         key3: 9,
+//     },
+// }
+//
+// let sum = 0
+//
+// for (let key in obj) {
+//     for (let key2 in obj[key]) {
+//         sum += obj[key][key2]
+//     }
+// }
+//
+// console.log(sum)
 
-console.log(arr);
+// let obj = {
+//     1: {
+//         1: 'a1',
+//         2: 'a2',
+//         3: 'a3',
+//     },
+//     2: {
+//         1: 'b1',
+//         2: 'b2',
+//         3: 'b3',
+//     },
+//     3: {
+//         1: 'c1',
+//         2: 'c2',
+//         3: 'c3',
+//     },
+// }
+// document.write(obj[2][1])
+// document.write(obj[3][1])
+// console.log(obj[2][2])
+// console.log(obj[3][1])
 
 
-
-
-
-
-
-
+// let obj = {
+//     key1: {
+//         a: 1, b: 2, c: {
+//             d: 3,
+//             e: 4,
+//         },
+//         f: 5,
+//     },
+//     key2: {
+//         g: 6, h: 7,
+//     },
+// }
+//
+// console.log(obj.key1.a +
+//     obj.key1.b +
+//     obj.key1.c.d +
+//     obj.key1.c.e +
+//     obj.key1.f +
+//     obj.key2.g +
+//     obj.key2.h
+// )
 
 
 
