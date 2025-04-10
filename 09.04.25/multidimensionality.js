4b01c834cda0e26b1fedcbb637bbecc6d4b77ff9
molnia_grom <mgrom250@gmail.com>
2025-04-10 13:07:12 +0300

    Array of objects in JavaScript

--- 09.04.25/multidimensionality.js
+++ 09.04.25/multidimensionality.js
@@ -541,14 +541,117 @@
 
 //........................................................................
 
+// let students = {
+//     'group1': ['student11', 'student12', 'student13'],
+//     'group2': ['student21', 'student22', 'student23'],
+//     'group3': ['student31', 'student32'],
+// };
+//
+// for (let group in students) {
+//     for (let name of students[group]) {
+//         console.log(name);
+//     }
+// }
 
+// let data = {
+//     1: [
+//         'data11',
+//         'data12',
+//         'data13',
+//     ],
+//     2: [
+//         'data21',
+//         'data22',
+//         'data23',
+//     ],
+//     3: [
+//         'data31',
+//         'data32',
+//         'data33',
+//     ],
+//     4: [
+//         'data41',
+//         'data42',
+//         'data43',
+//     ],
+// };
+//
+// for (let key in data) {
+//     for (let value of data[key]) {
+//         console.log(value);
+//     }
+// }
 
+// let data = [
+//     {
+//         1: 'data11',
+//         2: 'data12',
+//         3: 'data13',
+//     },
+//     {
+//         1: 'data21',
+//         2: 'data22',
+//         3: 'data33',
+//     },
+//     {
+//         1: 'data31',
+//         2: 'data32',
+//         3: 'data33',
+//     },
+// ];
+//
+// for (let key in data) {
+//     for (let value in data[key]) {
+//         console.log(data[key][value]);
+//     }
+// }
 
-
-
-
-
-
+// let data = [
+//     {
+//         1: [
+//             'data111',
+//             'data112',
+//             'data113',
+//         ],
+//         2: [
+//             'data121',
+//             'data122',
+//             'data123',
+//         ],
+//     },
+//     {
+//         1: [
+//             'data211',
+//             'data212',
+//             'data213',
+//         ],
+//         2: [
+//             'data221',
+//             'data222',
+//             'data223',
+//         ],
+//     },
+//     {
+//         1: [
+//             'data411',
+//             'data412',
+//             'data413',
+//         ],
+//         2: [
+//             'data421',
+//             'data422',
+//             'data423',
+//         ],
+//     },
+// ];
+//
+// for (let key in data) {
+//     for (let value in data[key]) {
+//         for (let value2 in data[key][value]) {
+//             console.log(data[key][value][value2]);
+//         }
+//     }
+// }
 
 
 
