9fd333731fa8df52a95c87993288fb9b646bf922
molnia_grom <mgrom250@gmail.com>
2025-04-10 13:17:44 +0300

    Keys from variables in JavaScript multidimensional structures

--- 09.04.25/multidimensionality.js
+++ 09.04.25/multidimensionality.js
@@ -653,6 +653,101 @@
 //     }
 // }
 
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//     },
+// ];
+//
+// for (let employee of employees) {
+//     console.log(` Имя: ${employee.name} Зарплата: ${employee.salary}`);
+// }
+
+
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//     },
+// ];
+//
+// let sumSalary = 0
+//
+// for (let employee of employees) {
+//     sumSalary += employee.salary
+// }
+//
+// console.log(sumSalary)
+
+
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//         age: 28,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//         age: 29,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//         age: 30,
+//     },
+//     {
+//         name: 'name4',
+//         salary: 600,
+//         age: 31,
+//     },
+//     {
+//         name: 'name5',
+//         salary: 700,
+//         age: 32,
+//     },
+// ];
+// let sumSalary = 0
+//
+// const verificationAge = age => age >= 30;
+//
+// for (let employee of employees) {
+//     if (verificationAge(employee.age)) {
+//         sumSalary += employee.salary
+//     }
+// }
+//
+// console.log(sumSalary)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
