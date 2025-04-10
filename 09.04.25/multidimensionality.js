391d612ce4ea5025b315a98219f5ce6044d65fd4
molnia_grom <mgrom250@gmail.com>
2025-04-10 11:53:10 +0300

    Multidimensional objects in JavaScript

--- 09.04.25/multidimensionality.js
+++ 09.04.25/multidimensionality.js
@@ -361,8 +361,18 @@
 //
 // console.log(arr);
 
+let arr = [];
 
+for (let i = 0, k = 1; i < 3;
+     i++) {
+    arr[i] = [];
 
+    for (let j = 0; j < 3; j++) {
+        arr[i][j] = k++;
+    }
+}
+
+console.log(arr);
 
 
 
