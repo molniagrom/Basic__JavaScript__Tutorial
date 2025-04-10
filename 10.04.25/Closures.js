c8eed2e56ea62f7d694eca23e9c90b70f1054115
molnia_grom <mgrom250@gmail.com>
2025-04-10 17:55:43 +0300

    Replacing parts of a string in JavaScript

---
+++ 10.04.25/Closures.js
@@ -0,0 +1,101 @@
+// console.log(Math.pow(2, 10));
+// console.log(2 ** 10);
+// console.log(Math.sqrt(245));
+
+// let arr = [4, 2, 5, 19, 13, 0, 10]
+// let sumCub = 0
+//
+// for (let i = 0; i < arr.length; i++) {
+//     sumCub += Math.pow(arr[i], 3)
+// }
+//
+// result = Math.sqrt(sumCub)
+//
+// console.log(result)
+
+// let squareRoot = Math.sqrt(379)
+// console.log(squareRoot.toFixed(3))
+// console.log(squareRoot.toFixed(2))
+// console.log(squareRoot.toFixed(1))
+// console.log(squareRoot.toFixed(0))
+
+// let squareRoot = Math.sqrt(587)
+//
+// let obj = {
+//     ceil: Math.ceil(squareRoot),
+//     floor: Math.floor(squareRoot)
+// }
+//
+// console.log(obj)
+
+
+// let numbers = [4, -2, 5, 19, -130, 0, 10];
+// console.log(Math.min(...numbers));
+// console.log(Math.max(...numbers));
+
+
+// console.log(Math.random())
+
+
+// function getRandomInt(min, max) {
+//
+//     return Math.floor(
+//         Math.random() *
+//         (max - min + 1)
+//     ) + min;
+// }
+//
+// console.log(getRandomInt(1, 1000));
+
+
+// let arr = []
+//
+// for (let i = 0; i < 10; i++) {
+//     arr.push(Math.floor(Math.random() * 1000))
+// }
+//
+// console.log(arr)
+
+// let a = -9
+// let b = 7
+//
+// console.log(
+//     Math.abs(a - b),
+// )
+
+// let jsSmall = "js"
+// let jsBig = "JS"
+//
+// console.log(jsSmall.toUpperCase())
+// console.log(jsBig.toLowerCase())
+
+// let str = 'я учу javascript!'
+//
+// console.log(str.substring(2, 6))
+// console.log(str.substring(6, 18))
+// console.log(str.slice(6, 18))
+// console.log(str.slice(2, 6))
+
+
+// let str = "https://code.mu/ru/javascript/book/prime/inbuilt/string/searching/"
+//
+// console.log(str.startsWith('http://'))
+// console.log(str.endsWith('.html'))
+// console.log(str.includes('book'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
