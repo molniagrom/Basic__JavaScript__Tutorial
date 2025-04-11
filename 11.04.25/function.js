6017afc0c0f2bdb3c120acc5f64a0b81310d1af5
molnia_grom <mgrom250@gmail.com>
2025-04-11 15:33:35 +0300

    cycle and return in JavaScript

--- 11.04.25/function.js
+++ 11.04.25/function.js
@@ -13,11 +13,114 @@
 //
 // sumNum(1, 100)
 
+// function cube(num) {
+//     return num ** 3
+// }
+//
+// console.log(cube(2))
 
+// function verification(num) {
+//     if (num > 0) {
+//         console.log('+++');
+//     } else {
+//         console.log('---');
+//     }
+// }
 
+// function sumNum(a, b, c) {
+//     console.log(a + b + c)
+// }
+//
+// sumNum(1, 2, 3)
 
+// let param1 = 1;
+// let param2 = 2;
+// let param3 = 3;
+//
+// function func(a, b, c){
+//     console.log(a + b + c)
+// }
+//
+// func(param1, param2, param3)
 
+// function func(num = 5) {
+//     console.log(num * num);
+// }
+//
+// func(2);
+// func(3);
+// func();
 
+// function func(num1 = 0, num2 = 0) {
+//     console.log(num1 + num2);
+// }
+//
+// func(2, 3);
+// func(3);
+// func();
+
+// function funcCube(num) {
+//     return num ** 3
+// }
+//
+// let res = funcCube(3)
+// console.log(res)
+
+// function squareRoot(a) {
+//     return Math.sqrt(a)
+// }
+//
+// console.log(squareRoot(3) + squareRoot(4))
+
+// function sqrt(num) {
+//     return Math.sqrt(num);
+// }
+//
+// function round(num) {
+//     return num.toFixed(3);
+// }
+//
+// console.log(
+//     round(sqrt(2))
+// )
+
+// function sqrt(num) {
+//     return Math.sqrt(num);
+// }
+//
+// function sum(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
+//
+// function round(num) {
+//     return num.toFixed(3);
+// }
+//
+// let res = round(sum(sqrt(2), sqrt(3), sqrt(4)))
+//
+// console.log(res)
+
+// function func(num) {
+//     if (num <= 0) {
+//         return Math.abs(num);
+//     } else {
+//         return num ** 2;
+//     }
+// }
+//
+// console.log( func(10) );
+// console.log( func(-5) );
+
+// function func(num) {
+//     if (num <= 0) {
+//         return Math.abs(num);
+//     }
+//
+//     return num ** 2;
+// }
+//
+// console.log( func(10) );
+// console.log( func(-5) );
 
 
 
