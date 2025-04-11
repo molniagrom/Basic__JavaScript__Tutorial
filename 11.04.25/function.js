1a925d9885ae8b12a0bceaa2f9b4f7f11220aba5
molnia_grom <mgrom250@gmail.com>
2025-04-11 14:59:18 +0300

    start function

---
+++ 11.04.25/function.js
@@ -0,0 +1,49 @@
+// function name(name) {
+//     console.log(name)
+// }
+//
+// name('Ivan')
+
+// function sumNum(a, b) {
+//     let sum = 0
+//     for (let i = a; i <= b; i++) {
+//         console.log(sum += a)
+//     }
+// }
+//
+// sumNum(1, 100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
