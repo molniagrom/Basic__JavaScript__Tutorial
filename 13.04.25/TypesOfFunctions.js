2c6655a80af604085e5952336db2985f5aa7aa27
molnia_grom <mgrom250@gmail.com>
2025-04-13 08:43:27 +0300

    Refactor and clean up function examples in script.
    Removed extraneous blank lines and added examples of various function types, including named and anonymous functions. Improved readability and structure for better understanding of function usage.

--- 13.04.25/TypesOfFunctions.js
+++ 13.04.25/TypesOfFunctions.js
@@ -13,20 +13,44 @@
 // funk = "rose"
 // console.log(funk)
 
+// function func1(){
+//     return 3
+// }
+//
+// func2 = func1
+// console.log(func2() + func1())
 
 
+// let func1 = function () {
+//     return 1
+// }
+// let func2 = function () {
+//     return 2
+// }
+//
+// alert(func1() + func2())
+// console.log(func1() + func2())
 
 
+// let func1 = function() {console.log('!')};
+// let func2 = function() {
+//     console.log('!')
+// };
+// function func3() {
+//     console.log('!')
+// }
 
+// function func() {
+//     console.log('!');
+// }
 
+func()
 
+function func() {
+    console.log('!');
+} + console.log('!');
 
-
-
-
-
-
-
+//Массив с анонимными функциями в JavaScript (237)
 
 
 
