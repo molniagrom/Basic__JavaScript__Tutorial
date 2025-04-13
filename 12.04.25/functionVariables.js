74a76e01538044c575ca0eb8d97d0ae1dfc0f179
molnia_grom <mgrom250@gmail.com>
2025-04-13 07:50:09 +0300

    Comment out unused function and related code
    The `func` function and its usage were commented out as they are not actively used. This helps in reducing noise and improving code readability without removing the code entirely for potential future use.

--- 12.04.25/functionVariables.js
+++ 12.04.25/functionVariables.js
@@ -43,14 +43,14 @@
 // func();
 // console.log(num);
 
-function func(arr) {
-    let newArr = arr;
-    newArr[0] = '!';
-}
-
-let arr = [1, 2, 3];
-func(arr);
-console.log(arr);
+// function func(arr) {
+//     let newArr = arr;
+//     newArr[0] = '!';
+// }
+//
+// let arr = [1, 2, 3];
+// func(arr);
+// console.log(arr);
 
 
 
