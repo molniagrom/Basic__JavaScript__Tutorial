27a542263defdac4581e4c49430e1326fe73b120
molnia_grom <mgrom250@gmail.com>
2025-04-13 07:49:46 +0300

    Types of functions

---
+++ 12.04.25/functionVariables.js
@@ -0,0 +1,74 @@
+// function func() {
+//     console.log(num);
+// }
+//
+// let num;
+//
+// num = 1;
+// func();
+//
+// num = 2;
+// func();
+
+// function func() {
+//     let num = 5;
+//     return num;
+// }
+//
+// console.log(func());
+
+// let num = 1;
+//
+// function func() {
+//     num = 2;
+// }
+// func();
+//
+// console.log(num);
+
+// let num = 1;
+//
+// function func() {
+//     let num = 2;
+// }
+// func();
+//
+// console.log(num);
+
+// function func() {
+//     num = 2;
+// }
+//
+// let num = 1;
+// func();
+// console.log(num);
+
+function func(arr) {
+    let newArr = arr;
+    newArr[0] = '!';
+}
+
+let arr = [1, 2, 3];
+func(arr);
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
