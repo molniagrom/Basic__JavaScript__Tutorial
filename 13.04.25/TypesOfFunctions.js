0204e2d11330bd9b381097d5337fa26726710c75
molnia_grom <mgrom250@gmail.com>
2025-04-13 08:14:07 +0300

    Types of functions (225 card)

---
+++ 13.04.25/TypesOfFunctions.js
@@ -0,0 +1,40 @@
+// function func() {
+//     return '!';
+// }
+//
+// console.log(func());
+
+// function func() {
+//     return 'addtrack lement to the cart';
+// }
+//
+// console.log(func);
+//
+// funk = "rose"
+// console.log(funk)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
