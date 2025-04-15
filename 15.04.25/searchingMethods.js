ba8b279137c3ea5d6f9e0eff42fe820bb4c784f9
molnia_grom <mgrom250@gmail.com>
2025-04-15 15:35:22 +0300

    every (282)

--- 15.04.25/searchingMethods.js
+++ 15.04.25/searchingMethods.js
@@ -23,10 +23,37 @@
 //.......................................................
 
 // let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//
+
 // console.log(arrNum.map((x, index) => x * index));
 
 //.......................................................
+
+// let sum = 0
+// arrNum.forEach(x => sum += x ** 2)
+// console.log(sum);
+
+//.......................................................
+
+// let mixedNumbers = [5, -3, 8, -1, 2, -7, 10, 50, -4];
+// let mixedStrings = ["hello", "world", "javascript", "code", "example"];
+// let arr = [1, 2, [3, 4], 5, [6, 7]]
+// console.log(mixedNumbers.filter(x => x > 0));
+// console.log(mixedNumbers.filter(x => x > 0 && x < 10));
+// console.log(mixedStrings.filter(x => x.length > 5));
+// console.log(mixedNumbers.filter((x, index) => (x * index) < 30));
+// console.log(arr.filter(x => !Array.isArray(x)));
+
+//.......................................................
+
+
+
+
+
+
+
+
+
+
 
 
 
