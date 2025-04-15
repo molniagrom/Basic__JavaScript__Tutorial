e6bc7ff1f14bd22c17454c6a100d853ab241ba27
molnia_grom <mgrom250@gmail.com>
2025-04-15 14:20:34 +0300

    Add commented examples of array manipulation methods
    This commit includes commented-out examples demonstrating the use of `map()` for various array transformations. These examples cover numerical arrays, string arrays, and nested operations to showcase different use cases.

---
+++ 15.04.25/searchingMethods.js
@@ -0,0 +1,48 @@
+// let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(arrNum.map(x => x ** 2));
+
+// let sqrtArr = arrNum.map(x => Math.sqrt(x));
+// console.log(sqrtArr);
+
+//.......................................................
+
+// let arrStr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+// let arrFruits = ["apple", "banana", "cherry", "mango", "orange"];
+
+// console.log(arrStr.map(x => x + "!"));
+// console.log(arrFruits.map(x => x.split("").reverse().join("")));
+
+//.......................................................
+
+// let arr = ['123', '456', '789'];
+//
+// console.log(arr.map(x =>
+//     x.split('').map(y => Number(y))));
+
+//.......................................................
+
+// let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(arrNum.map((x, index) => x * index));
+
+//.......................................................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
