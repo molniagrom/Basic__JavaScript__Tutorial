0a6f4854f05b52066a0920db103748c1c5a3eb5c
molnia_grom <mgrom250@gmail.com>
2025-04-16 12:08:36 +0300

    end spread operator

---
+++ 15.04.25/operatorSpreadAndRest.js
@@ -0,0 +1,79 @@
+// let arr = [1, 2, 3, 4, 5];
+//
+// function func(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+//
+// console.log(func(...arr))
+
+//....................................................
+
+// function func(n1, n2, n3, n4, n5,
+//               n6, n7, n8) {
+//     return (n1 + n2 + n3 + n4) * (n5
+//         + n6 + n7 + n8);
+// }
+//
+// console.log( func(1, ...[2, 3,
+//     4], 5, ...[6], ...[7, 8]) );
+
+//....................................................
+
+// let arr = [1, 2, 3, 4, 5]
+// let min = Math.min(...arr)
+// console.log(min)
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [...arr1, 4, 5, 6];
+// let arr3 = [...arr2, 7, 8, 9];
+//
+// let arr  = [0, ...arr3];
+// console.log(arr);
+
+//.......................................................
+
+// let arr1 = ['a', 'b', 'c'];
+// let arr2 = [...arr1, ...'12345'];
+
+// console.log(arr2);
+
+//.......................................................
+
+// function arithmeticMean(...nums) {
+//
+//     let sum = 0;
+//
+//     for (let num of nums) {
+//         sum += num;
+//     }
+//     return Number(((sum / nums.length) * 10).toFixed(1))
+// }
+//
+// console.log(arithmeticMean(...[5, 4, 2, 3, 4, 5]));
+
+// .......................................................
+
+// function unite(...arrs) {
+//     return arrs;
+// }
+//
+// let result = unite([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+// console.log(result);
+
+//.......................................................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
