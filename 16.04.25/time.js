f77961493ce77f96848d9d799145878b49d8fe61
molnia_grom <mgrom250@gmail.com>
2025-04-17 15:30:27 +0300

    Time (315)

---
+++ 16.04.25/time.js
@@ -0,0 +1,65 @@
+let date = new Date();
+//
+// console.log(date)
+//
+// let year = String(zeroAdd(date.getFullYear()));
+// let day = String(zeroAdd(date.getDate()));
+// let month = String(zeroAdd(date.getMonth()));
+//
+// let hour = String(zeroAdd(date.getHours()));
+// let minute = String(zeroAdd(date.getMinutes()));
+// let second = String(zeroAdd(date.getSeconds()));
+//
+// function zeroAdd(num){
+//     if (num >= 0 && num <= 9) {
+//         return `0${num}`
+//     } else {
+//         return num
+//     }
+// }
+//
+// document.write(`${hour}.${minute}.${second}`)
+// document.write(`${day}.${month}.${year}`)
+
+
+// let str = '2025-12-31';
+// console.log(str.split("-").reverse().join("."));
+
+// function workOrWeekend(num) {
+//     if (num === 0 || num === 6) {
+//       return "Выходной"
+//     } else {
+//         return "Будний"
+//     }
+// }
+//
+// console.log(workOrWeekend(date.getDay()))
+// console.log(date.getDay())
+// console.log("До ближайшего воскресенья осталось: " + (6 - date.getDay()) + "дня")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
