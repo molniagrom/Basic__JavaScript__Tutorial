c605dadaa26f1d9364d1ef57a8747fe1be437b39
molnia_grom <mgrom250@gmail.com>
2025-04-17 19:16:04 +0300

    dom (344)

---
+++ 16.04.25/dom.js
@@ -0,0 +1,66 @@
+// let elem1 = document.querySelector("#elem1");
+// let elem2 = document.querySelector("#elem2");
+// let elem3 = document.querySelector("#elem3");
+//
+// console.log(elem1.textContent);
+// console.log(elem2.textContent);
+// console.log(elem3.textContent);
+
+// let block = document.querySelector(".block p");
+// console.log(block.textContent);
+
+// let p = document.querySelector(".www");
+// console.log(p.textContent);
+
+//....................................................
+
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+// let button3 = document.querySelector("#button3");
+//
+// button1.addEventListener("click", function () {
+//     alert(1);
+// })
+// button2.addEventListener("click", function () {
+//     alert(2);
+// })
+// button3.addEventListener("click", function () {
+//     alert(3);
+// })
+
+//....................................................
+
+// function func1() {
+//     console.log(1);
+// }
+//
+// function func2() {
+//     console.log(2);
+// }
+//
+// button1.addEventListener("click", func1);
+// button2.addEventListener("click", func2);
+
+//....................................................
+
+// function func() {
+//     console.log('message');
+// }
+//
+// for (let i = 1; i <= 5; i++) {
+//     let elem = document.getElementById('elem' + i);
+//     elem.addEventListener('click', func);
+// }
+
+//....................................................
+
+
+
+
+
+
+
+
+
+
+
